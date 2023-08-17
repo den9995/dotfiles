00a1e2a7c9b54a287016a97047f8d5dbf22da0e1
den9995 <den9995@gmail.com>
2023-08-17 13:23:58 +0300

    added distribution 	modified:   collect.sh 	new file:   distribute.sh

---
+++ distribute.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+src_dir=$(dirname "$0")
+config_dir=/home/user/.config
+cp -r $src_dir/alacritty  $config_dir
+cp -r $src_dir/keepmenu   $config_dir
+cp -r $src_dir/lf         $config_dir
+cp -r $src_dir/mpv        $config_dir
+cp -r $src_dir/nvim       $config_dir
+cp -r $src_dir/qtile      $config_dir
+cp -r $src_dir/rofi       $config_dir
+cp -r $src_dir/picom.conf $config_dir
+#cp -r $src_dir/           $config_dir
